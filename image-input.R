69c67d320dc887981411ee8d027d503d4008cd0e
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2025-03-11 13:31:56 +1100

    remove image_description_app & test images
    moved into stand-alone repo

--- image-input.R
+++ image-input.R
@@ -2,21 +2,6 @@ library(ellmer)
 
 chat <- chat_gemini()
 
-image_path <- "images/diagram_PRITS-framework.png"
-
-chat$chat(
-  content_image_file(image_path),
-  "Describe this diagram"
-)
-
-chat <- chat_gemini()
-image_path <- "images/how-to-pitch_worklife-adam-grant.png"
-chat$chat(
-  content_image_file(image_path),
-  # "Describe this diagram",
-  "Describe this image"
-)
-
 chat$chat(
   content_image_file("holly-mandarich-3p9zaNwUtv8-unsplash.jpg"),
   "Describe this photo"
