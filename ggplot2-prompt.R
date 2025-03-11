888e8e33b3acb887ffc79c9b1bd7df319c4c5c6e
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2025-03-11 12:31:20 +1100

    add attempt at ggplot2 task, notes on system vs user prompt

--- ggplot2-prompt.R
+++ ggplot2-prompt.R
@@ -4,6 +4,12 @@ iris_glimpse <- paste0(capture.output(dplyr::glimpse(iris)), collapse = "\n")
 diamonds_glimpse <- paste0(capture.output(dplyr::glimpse(ggplot2::diamonds)), collapse = "\n")
 
 prompt <- interpolate_file("ggplot2-prompt.md", glimpse = diamonds_glimpse)
+cat(prompt)
 
-chat <- chat_claude()
+chat <- chat_gemini()
 chat$chat(prompt)
+
+## -- ASIDE: what do you put into system prompt vs. user chat prompt?;
+# - HW: don't know?; system prompt is "stronger" -- trunk of tree
+# HW: openAI has hierarchy of prompts -- that even the system prompt underneath
+# MOW: system prompt forgetten? -- HW: newer models are trying to prevent loss of system prompt
--- ggplot2-prompt.md
+++ ggplot2-prompt.md
@@ -1,11 +1,13 @@
 You are an expert ggplot2 programmer. 
 
 Help me brainstorm ways to visualise the <dataset> described below. 
-Give me 10 different places to get started.
+Give me 3 different places to get started.
 
 * Be creative! 
 * Return the results as a single block of code, using comments to separate the 
   plots
+* Do not use any tidyverse packages  (i.e., dplyr, tidyr, ggplot2, etc.)
+* Use `theme_light()`
 
 <dataset>
 {{glimpse}}
