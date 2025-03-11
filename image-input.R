765eab91ef131095cabf93a05c505a65b7231210
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2025-03-11 12:31:47 +1100

    add inputs to try image description with gemini

--- image-input.R
+++ image-input.R
@@ -1,6 +1,21 @@
 library(ellmer)
 
-chat <- chat_claude()
+chat <- chat_gemini()
+
+image_path <- "images/diagram_PRITS-framework.png"
+
+chat$chat(
+  content_image_file(image_path),
+  "Describe this diagram"
+)
+
+chat <- chat_gemini()
+image_path <- "images/how-to-pitch_worklife-adam-grant.png"
+chat$chat(
+  content_image_file(image_path),
+  # "Describe this diagram",
+  "Describe this image"
+)
 
 chat$chat(
   content_image_file("holly-mandarich-3p9zaNwUtv8-unsplash.jpg"),
